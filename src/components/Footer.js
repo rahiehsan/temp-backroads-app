3fae8b4c1e2ec909c0528cb3761ec67c79421903
rahiehsan <rahiehsan045@gmail.com>
2024-03-06 14:58:16 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
+import SocialLinks from "./SocialLinks";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          // const { id, href, icon } = link;
+          return (
+            <SocialLinks key={link.id} {...link} itemClass="footer-icon" />
+          );
+        })}
+      </ul>
+
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
